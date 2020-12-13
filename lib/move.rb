e5691376cf2d380b6e6e73f1b375094029fb6aea
Bertrand Halaby <halabb11@gmail.com>
2020-12-13 22:47:47 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,5 +8,9 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index(input)
-converted_input = input_to_index(input)
+input .to_i - 1
+end
+
+def move(board, index, marker)
+  board[index] = marker
 end
