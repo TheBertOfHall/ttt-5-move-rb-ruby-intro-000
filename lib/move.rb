f5fa3de4ad60aa4bfba44224d6426e8cc31a57f0
Bertrand Halaby <halabb11@gmail.com>
2020-12-14 00:00:42 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -15,10 +15,18 @@ def move(board, index, marker = "X")
   board[index] = marker
 end
 
-def position_taken?(board, index)
-   if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
-      return false
-   else
-      return true
-   end
+def position_taken? (board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && (index).between?(0,8)
+    return true
+  else
+    return false
+  end
 end
